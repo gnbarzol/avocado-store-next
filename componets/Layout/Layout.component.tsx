8983fd0090409504cd137085f8e9eaffa81673f0
Gary Barzola <gnbarzol@MacBook-Pro-de-Gary.local>
2020-10-01 18:54:11 -0500

    Add css-in-js: style jsx

--- componets/Layout/Layout.component.tsx
+++ componets/Layout/Layout.component.tsx
@@ -1,14 +1,18 @@
 import React, { ReactNode } from "react";
-import Navbar from '../Navbar/Navbar';
-
-import styles from './Layout.module.css';
+import Navbar from "../Navbar/Navbar";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div className={styles.container}>
+    <div>
       <Navbar />
       {children}
-      <footer>This is the footer</footer>
+      <footer className="container">This is the footer</footer>
+
+      <style jsx>{`
+        .container: {
+          background: salmon;
+        }
+      `}</style>
     </div>
   );
 };
